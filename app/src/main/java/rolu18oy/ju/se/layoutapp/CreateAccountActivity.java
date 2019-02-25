e2f3ed9d6601c19e4d1605440c4e829c7772a849
Kanimba <ekanimba3@gmail.com>
2019-02-25 20:42:30 +0100

    Added Strings in user Model

--- app/src/main/java/rolu18oy/ju/se/layoutapp/CreateAccountActivity.java
+++ app/src/main/java/rolu18oy/ju/se/layoutapp/CreateAccountActivity.java
@@ -1,10 +1,8 @@
 package rolu18oy.ju.se.layoutapp;
-
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,11 +22,20 @@ public class CreateAccountActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference users;
 
-    EditText edtPassword, edtMail;
+    EditText edtPassword, edtMail, edtfirstname , edtlastname;
     Button btnCreateAccount;
 
-    private String userId;
-    private DatabaseReference mFirebaseDatabase;
+    public void init(){
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        edtMail = (EditText) findViewById(R.id.username);
+        edtPassword = (EditText) findViewById(R.id.password);
+        edtfirstname = (EditText) findViewById(R.id.first_name);
+        edtlastname = (EditText) findViewById(R.id.last_name);
+
+        btnCreateAccount = (Button) findViewById(R.id.signup);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,22 +46,20 @@ public class CreateAccountActivity extends AppCompatActivity {
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                final User user = new User(
-                        edtPassword.getText().toString(),
-                        edtMail.getText().toString().replace(".",","));
-
+                Log.i("check", edtMail.getText().toString());
+                System.out.print(edtMail.getText().toString());
+                final User user = new User(edtfirstname.getText().toString(),
+                        edtlastname.getText().toString(),
+                        edtMail.getText().toString(),
+                        edtPassword.getText().toString());
                 users.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
                         if(dataSnapshot.child(user.getEmail()).exists()){
                             Toast.makeText(CreateAccountActivity.this, "The Email already exits \n Email ", Toast.LENGTH_SHORT).show();
                         }
-                        else {
+                        else{
                             users.child(user.getEmail()).setValue(user);
-                            Toast.makeText(CreateAccountActivity.this,"Success Register!", Toast.LENGTH_SHORT).show();
-                            finish();
                         }
                     }
                     @Override
@@ -66,56 +71,11 @@ public class CreateAccountActivity extends AppCompatActivity {
         });
     }
 
+
     public boolean PasswordCheck(String password0, String password1){
         if(password0 == password1){
             return true;
         }
         return false;
-    }
-
-    public void init(){
-        database = FirebaseDatabase.getInstance();
-        users = database.getReference("Users");
-
-        edtMail = (EditText) findViewById(R.id.username);
-        edtPassword = (EditText) findViewById(R.id.password);
-        btnCreateAccount = (Button) findViewById(R.id.signup);
-    }
-
-    private void createUser(String email, String password) {
-
-        if (TextUtils.isEmpty(userId)) {
-            userId = mFirebaseDatabase.push().getKey();
-        }
-
-        User user = new User(email, password);
-
-        mFirebaseDatabase.child(userId).setValue(user);
-
-        addUserChangeListener();
-    }
-
-    private void addUserChangeListener() {
-        // User data change listener
-        mFirebaseDatabase.child(userId).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue(User.class);
-
-                // Check for null
-                if (user == null) {
-                    Log.e(TAG, "User data is null!");
-                    return;
-                }
-
-                Log.e(TAG, "User data is changed!" + user.getEmail() + ", " + user.getPassword());
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.e(TAG, "Failed to read user", error.toException());
-            }
-        });
     }
 }
--- app/src/main/java/rolu18oy/ju/se/layoutapp/Model/User.java
+++ app/src/main/java/rolu18oy/ju/se/layoutapp/Model/User.java
@@ -4,26 +4,49 @@ public class User {
 
     private  String email;
     private  String password;
+    private  String FirstName;
+    private  String LastName;
 
-
-    public User(){
-
-    }
-
-    public User(String email, String password){
-        this.password = password;
-        this.email = email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword() {
-        this.password = password;
-    }
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
-    public void setEmail() {
+
+    public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public User(){
+    }
+
+    public User(String firstname,String lastname, String email, String password){
+        this.password = password;
+        this.email = email;
+        this.FirstName = firstname;
+        this.LastName = lastname;
+    }
+
 }
