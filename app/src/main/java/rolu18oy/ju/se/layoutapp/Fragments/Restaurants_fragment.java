97bb2c06d403492d2a3388cf37ec1dcb82d118f2
lucas <lucas_correia@hotmail.com>
2019-02-27 00:02:46 +0100

    Fragments done

---
+++ app/src/main/java/rolu18oy/ju/se/layoutapp/Fragments/Restaurants_fragment.java
@@ -0,0 +1,19 @@
+package rolu18oy.ju.se.layoutapp.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import rolu18oy.ju.se.layoutapp.R;
+
+
+public class Restaurants_fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.restaurants_fragment, null);
+    }
+}
--- app/src/main/java/rolu18oy/ju/se/layoutapp/MainActivity.java
+++ app/src/main/java/rolu18oy/ju/se/layoutapp/MainActivity.java
@@ -1,18 +1,64 @@
 package rolu18oy.ju.se.layoutapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import rolu18oy.ju.se.layoutapp.Fragments.Booked_fragment;
+import rolu18oy.ju.se.layoutapp.Fragments.Restaurants_fragment;
+import rolu18oy.ju.se.layoutapp.Fragments.User_fragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_account);
+        setContentView(R.layout.navigation_activity);
 
-        Intent intent = new Intent(getApplicationContext(), CreateAccountActivity.class);
-        startActivity(intent);
+        //loading the default fragment
+        loadFragment(new Restaurants_fragment());
 
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_restaurants:
+                fragment = new Restaurants_fragment();
+                break;
+
+            case R.id.navigation_booked:
+                fragment = new Booked_fragment();
+                break;
+
+            case R.id.navigation_user:
+                fragment = new User_fragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
