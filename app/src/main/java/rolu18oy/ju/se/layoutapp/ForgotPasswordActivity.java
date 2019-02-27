4e54f24d0eba7d64f32f017f6b6ac3197e00a5fd
lucas <lucas_correia@hotmail.com>
2019-02-27 12:56:54 +0100

    Forgot pass working

--- app/src/main/java/rolu18oy/ju/se/layoutapp/ForgotPasswordActivity.java
+++ app/src/main/java/rolu18oy/ju/se/layoutapp/ForgotPasswordActivity.java
@@ -1,13 +1,63 @@
 package rolu18oy.ju.se.layoutapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    ProgressBar progressBar;
+    EditText userEmail;
+    Button userPass;
+
+    FirebaseAuth firebaseAuth;
+
+    public void init(){
+
+        userEmail = (EditText) findViewById(R.id.edtUserEmail);
+        userPass = findViewById(R.id.btnforgotPass);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgot_password);
+
+        init();
+
+        userPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = userEmail.getText().toString();
+
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(ForgotPasswordActivity.this, "Password send to your email",Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(ForgotPasswordActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 }
--- app/src/main/java/rolu18oy/ju/se/layoutapp/Fragments/User_fragment.java
+++ app/src/main/java/rolu18oy/ju/se/layoutapp/Fragments/User_fragment.java
@@ -1,20 +1,35 @@
 package rolu18oy.ju.se.layoutapp.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import rolu18oy.ju.se.layoutapp.ForgotPasswordActivity;
 import rolu18oy.ju.se.layoutapp.R;
 
 
 public class User_fragment extends Fragment {
+    Button btnChangePass;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_fragments, null);
+        View view = inflater.inflate(R.layout.user_fragments,null);
+        btnChangePass = (Button) view.findViewById(R.id.btnChangePass);
+
+        btnChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
 }
