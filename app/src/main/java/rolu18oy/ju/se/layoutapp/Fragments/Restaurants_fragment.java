8894cc18b891583a2684995f964061660e5c1b38
lucas <lucas_correia@hotmail.com>
2019-03-05 18:45:00 +0100

    Added simple list recyclerview to restaurants

--- app/src/main/java/rolu18oy/ju/se/layoutapp/Fragments/Restaurants_fragment.java
+++ app/src/main/java/rolu18oy/ju/se/layoutapp/Fragments/Restaurants_fragment.java
@@ -1,19 +1,110 @@
 package rolu18oy.ju.se.layoutapp.Fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import rolu18oy.ju.se.layoutapp.R;
+import rolu18oy.ju.se.layoutapp.RecyclerViewAdapter;
 
 
 public class Restaurants_fragment extends Fragment {
+    View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.restaurants_fragment, null);
+        view = inflater.inflate(R.layout.testfragmentrestaurant, null);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        populateData();
+        initAdapter();
+        initScrollListener();
+        return view;
+    }
+
+
+
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+    ArrayList<String> rowsArrayList = new ArrayList<>();
+
+
+    boolean isLoading = false;
+
+
+
+    private void populateData() {
+        int i = 0;
+        while (i < 10) {
+            rowsArrayList.add("Restaurant " + i);
+            i++;
+        }
+    }
+
+    private void initAdapter() {
+
+        recyclerViewAdapter = new RecyclerViewAdapter(rowsArrayList);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    private void initScrollListener() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == rowsArrayList.size() - 1) {
+                        loadMore();
+                        isLoading = true;
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    private void loadMore() {
+        rowsArrayList.add(null);
+        recyclerViewAdapter.notifyItemInserted(rowsArrayList.size() - 1);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                rowsArrayList.remove(rowsArrayList.size() - 1);
+                int scrollPosition = rowsArrayList.size();
+                recyclerViewAdapter.notifyItemRemoved(scrollPosition);
+                int currentSize = scrollPosition;
+                int nextLimit = currentSize + 10;
+
+                while (currentSize - 1 < nextLimit) {
+                    rowsArrayList.add("Restaurant " + currentSize);
+                    currentSize++;
+                }
+
+                recyclerViewAdapter.notifyDataSetChanged();
+                isLoading = false;
+            }
+        }, 3000);
+
+
     }
 }
