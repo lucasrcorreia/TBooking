a44242e63ac7f9e739dfa3051f5aa7a650d01a64
lucas <lucas_correia@hotmail.com>
2019-03-11 15:07:56 +0100

    nothing changed

--- app/src/main/java/rolu18oy/ju/se/layoutapp/Fragments/Restaurants_fragment.java
+++ app/src/main/java/rolu18oy/ju/se/layoutapp/Fragments/Restaurants_fragment.java
@@ -6,10 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -39,7 +41,7 @@ public class Restaurants_fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.activity_restaurant, null);
+        view = inflater.inflate(R.layout.restaurant_fragment, null);
 
         mRecyclerView  = (RecyclerView) view.findViewById(rolu18oy.ju.se.layoutapp.R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
